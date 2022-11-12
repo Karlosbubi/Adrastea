668db1e4891c9a9c243ba324ec2932ce7526a71a
Kurt Brunner <Kontakt@Karlosbubi.de>
2022-11-12 17:21:10 +0100

    architecting base

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+struct Feed {
+    id: i128,
+    name: String,
+    url: String,
+}
+struct Article {
+    title: String,
+    version: i32,
+    path: String,
+    feed: Feed,
+    url: String,
+}
+
+struct Settings {
+    feeds: Vec<Feed>,
+    check_every: i32,
+    save_root: String,
+    config_path: String,
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+impl Settings {
+    pub fn new() -> Self {
+        Settings {
+            feeds: Vec::new(),
+            check_every: 5,
+            save_root: "$HOME/.adrastea".to_string(),
+            config_path: "$HOME/.config/adrastea.json".to_string(),
+        }
+    }
 }
